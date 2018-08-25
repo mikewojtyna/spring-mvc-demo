50dbb4d24b4926e3c0b0700741eb85cd78624533
Mike Wojtyna <mike@buildmysoftware.pro>
2018-08-25 23:01:31 +0200

    Create project

---
+++ src/main/java/pl/sdacademy/spring/mvcdemo/Greeting.java
@@ -0,0 +1,24 @@
+package pl.sdacademy.spring.mvcdemo;
+
+public class Greeting {
+
+	private long id;
+	private String content;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
---
+++ src/test/java/pl/sdacademy/spring/mvcdemo/GreetingControllerIntegrationTest.java
@@ -0,0 +1,53 @@
+package pl.sdacademy.spring.mvcdemo;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet
+	.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.springframework.test.web.servlet.request
+	.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request
+	.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result
+	.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result
+	.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result
+	.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GreetingControllerIntegrationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void getGreeting() throws Exception {
+		mockMvc.perform(get("/greeting")).andExpect(status().isOk())
+			.andExpect(model().attributeExists("greeting"))
+			.andExpect(model().attribute("greeting", hasProperty
+				("content", isEmptyOrNullString()))).andDo
+			(print());
+	}
+
+	@Test
+	public void postGreeting() throws Exception {
+		mockMvc.perform(post("/greeting").param("content", "hello " +
+			"world!")).andExpect(status().isOk()).andExpect(model
+			().attributeExists("greeting")).andExpect(model()
+			.attribute("greeting", hasProperty("content", is
+				("hello world!")))).andDo(print());
+	}
+
+}
